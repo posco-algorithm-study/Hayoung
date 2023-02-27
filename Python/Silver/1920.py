e26011b46fafd12a5e7b7a534db5ac775afc9447
hayoung <hayoung3788@gmail.com>
2023-02-27 17:25:02 +0900

    1920-수 찾기

---
+++ Python/Silver/1920.py
@@ -0,0 +1,12 @@
+import sys
+
+n = int(input())
+n_list = set(map(int, input().split()))
+m = int(input())
+m_list = list(map(int, input().split()))
+
+for i in m_list:
+    if i in n_list:
+        print(1)
+    else:
+        print(0)
