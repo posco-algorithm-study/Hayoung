82b01e665ece5ab5da37574ec18d982099aa83c7
hayoung <hayoung3788@gmail.com>
2023-03-03 14:30:28 +0900

    12015-LIS

---
+++ Python/Gold/12015.py
@@ -0,0 +1,20 @@
+n = int(input())
+cases = list(map(int, input().split()))
+lst = [0]
+
+for case in cases:
+    if lst[-1] < case:
+        lst.append(case)
+    else:
+        start = 0
+        end = len(lst)
+        
+        while start < end:
+            mid = (start + end) // 2
+            if lst[mid] < case:
+                start = mid + 1
+            else:
+                end = mid
+        lst[end] = case
+
+print(len(lst)-1)
