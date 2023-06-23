7b13672a8fbc776cd10bfd01e94d2717a9bd723d
hayoung <hayoung3788@gmail.com>
2023-06-23 15:27:14 +0900

    1475-방 번호

---
+++ Python/Silver/1475.py
@@ -0,0 +1,14 @@
+n = input()
+
+card = [0] * 10
+
+for i in n:
+    if i == "6" or i =="9":
+        if card[6] == card[9]:
+            card[6] += 1
+        else:
+            card[9] += 1
+    else:
+        card[int(i)] += 1
+
+print(max(card))
