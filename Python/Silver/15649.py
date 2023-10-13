ff810563db965339b8b206de7d62e04414a19e3f
hayoung <hayoung3788@gmail.com>
2023-10-13 13:02:33 +0900

    15649-N과 M(1)

--- Python/Silver/15649.py
+++ Python/Silver/15649.py
@@ -11,3 +11,9 @@ def back():
             back()
             arr.pop() # return으로 돌아오면 실행. 1, 2, 3 일 때 3을 없앰으로 전 단계로 돌아가는 것
 back()
+
+# from itertools import permutations
+# n, m = map(int, input().split())
+# p = permutations(range(1, n+1), m)
+# for i in p:
+#     print(" ".join(map(str, i)))
