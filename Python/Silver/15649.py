9faa75750f81aa86121bf6eedf4d6b053d7a7e9c
hayoung <hayoung3788@gmail.com>
2023-10-13 11:27:34 +0900

    15649-N과 M(1)

---
+++ Python/Silver/15649.py
@@ -0,0 +1,13 @@
+n, m = map(int, input().split())
+arr = []
+
+def back():
+    if len(arr) == m: # 배열의 길이 확인
+        print(" ".join(map(str, arr))) # 각 수열을 공백으로 구분해서 출력하기 위해
+        return
+    for i in range(1, n+1):
+        if i not in arr: # 중복 확인
+            arr.append(i) # 배열 추가
+            back()
+            arr.pop() # return으로 돌아오면 실행. 1, 2, 3 일 때 3을 없앰으로 전 단계로 돌아가는 것
+back()
