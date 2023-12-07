09012e2823ad26530fbda7fe99498d3eec9904f2
hayoung <hayoung3788@gmail.com>
2023-12-07 22:17:06 +0900

    2847-게임을 만든 동준이

---
+++ Python/Silver/2847.py
@@ -0,0 +1,13 @@
+n = int(input())
+score = []
+
+for _ in range(n):
+    score.append(int(input()))
+    
+cnt = 0
+for i in range(n-2, -1, -1):
+    if score[i] >= score[i+1]:
+        cnt += score[i] - score[i+1] + 1
+        score[i] = score[i+1] -1
+
+print(cnt)
